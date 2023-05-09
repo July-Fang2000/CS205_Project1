1d6b88a89ba50a376d8e3101976d021332d382f6
July-Fang2000 <zfang052@ucr.edu>
2023-05-08 22:54:01 -0700

    add: two methods about heurisitc

--- method.py
+++ method.py
@@ -1,9 +1,23 @@
-def uniform_cost_search():
+# def uniform_cost_search():
 
 
-def a_star_with_misplaced_tile_heuristic():
+def a_star_with_misplaced_tile_heuristic(current_puzzle, goal_puzzle):
+    distance = 0
+    size = len(current_puzzle)
+    for i in range(size):
+        for j in range(size):
+            if current_puzzle[i][j] != goal_puzzle[i][j] and current_puzzle[i][j] != 0:
+                distance += 1
+    return distance
 
 
-def a_star_with_manhattan_distance_heuristic():
-
-
+def a_star_with_manhattan_distance_heuristic(current_puzzle, goal_puzzle):
+    distance = 0
+    size = len(current_puzzle)
+    for i in range(size):
+        for j in range(size):
+            value = current_puzzle[i][j]
+            if value != 0:
+                row_goal, col_goal = divmod(goal_puzzle.index(value), size)
+                distance += abs(row_goal - i) + abs(col_goal - j)
+    return distance
