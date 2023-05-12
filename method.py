2be2cac6aebbb099cba971ca82a2147b7eb7995a
July-Fang2000 <zfang052@ucr.edu>
2023-05-11 23:09:56 -0700

    add: prepare for search to get the heuristic algorithm value of current puzzle fix: method function parameters name

--- method.py
+++ method.py
@@ -6,33 +6,42 @@ def uniform_cost_search():
     return cost
 
 
-def a_star_with_misplaced_tile_heuristic(current_puzzle, goal_puzzle):
+def a_star_with_misplaced_tile_heuristic(original_puzzle, goal_puzzle):
     distance = 0
-    size = len(current_puzzle)
+    size = len(original_puzzle)
     for i in range(size):
         for j in range(size):
-            if current_puzzle[i][j] != goal_puzzle[i][j] and current_puzzle[i][j] != 0:
+            if original_puzzle[i][j] != goal_puzzle[i][j] and original_puzzle[i][j] != 0:
                 distance += 1
     return distance
 
 
-def a_star_with_manhattan_distance_heuristic(current_puzzle, goal_puzzle):
+def a_star_with_manhattan_distance_heuristic(original_puzzle, goal_puzzle):
     distance = 0
-    size = len(current_puzzle)
+    size = len(original_puzzle)
     for i in range(size):
         for j in range(size):
-            value = current_puzzle[i][j]
+            value = original_puzzle[i][j]
             if value != 0:
                 row_goal, col_goal = divmod(goal_puzzle.index(value), size)
                 distance += abs(row_goal - i) + abs(col_goal - j)
     return distance
 
 
-def search(original_puzzle, goal_puzzle, method):
+def search(original_puzzle, goal_puzzle, solve_method):
     # find the blank position in original_puzzle
     blank_col, blank_row = 0, 0
     for i in range(len(original_puzzle)):
         for j in range(len(original_puzzle)):
             if original_puzzle[i][j] == 0:
                 blank_row, blank_col = i, j
-    print(blank_row, blank_col)
+
+    current_puzzle = Node(original_puzzle, blank_row, blank_col)
+
+    # use the method to get h value come from heuristic algorithm
+    if solve_method == 1:
+        current_puzzle.hcost = uniform_cost_search()
+    elif solve_method == 2:
+        current_puzzle.hcost = a_star_with_misplaced_tile_heuristic(original_puzzle, goal_puzzle)
+    elif solve_method == 3:
+        current_puzzle.hcost = a_star_with_manhattan_distance_heuristic(original_puzzle, goal_puzzle)
