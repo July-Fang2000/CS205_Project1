7d73b002ae5f15386b836f07b57bfcb20ff476be
July-Fang2000 <zfang052@ucr.edu>
2023-05-12 00:05:07 -0700

    add: use heapq to do search and update g and h value each time after expand the node.

--- method.py
+++ method.py
@@ -1,3 +1,5 @@
+import heapq
+
 from common import *
 
 
@@ -45,3 +47,41 @@ def search(original_puzzle, goal_puzzle, solve_method):
         current_puzzle.hcost = a_star_with_misplaced_tile_heuristic(original_puzzle, goal_puzzle)
     elif solve_method == 3:
         current_puzzle.hcost = a_star_with_manhattan_distance_heuristic(original_puzzle, goal_puzzle)
+
+    queue = [current_puzzle]
+    old_puzzle = [original_puzzle]
+
+    while True:
+        # if no more nodes can test, this puzzle problem can't solve.
+        if len(queue) == 0:
+            return "Can't solve this puzzle!"
+
+        # get and pop the first puzzle in the heap queue
+        q = heapq.heappop(queue)
+
+        # if it gets the goal, end the loop
+        if q.puzzle == goal_puzzle:
+            return
+
+        # expand and get its four neighbors of the node
+        new_puzzle, pre = generate_puzzle(q, old_puzzle)
+
+        # update the g and h of each algorithm
+        for i in pre:
+            if i is not None:
+                if solve_method == 1:
+                    i.g = q.g + 1
+                elif solve_method == 2:
+                    i.g = q.g + 1
+                    i.h = a_star_with_misplaced_tile_heuristic(i.puzzle, goal_puzzle)
+                elif solve_method == 3:
+                    i.g = q.g + 1
+                    i.h = a_star_with_manhattan_distance_heuristic(i.puzzle, goal_puzzle)
+                if i.puzzle not in old_puzzle:
+                    heapq.heappush(queue, i)
+                    old_puzzle.append(i.puzzle)
+
+
+def generate_puzzle(current_puzzle, old_puzzle):
+    arr = []
+    return current_puzzle, arr
