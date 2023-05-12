419f543a92a54e94b9c8433c822df118cc1a392b
July-Fang2000 <zfang052@ucr.edu>
2023-05-11 19:49:52 -0700

    add: method to create the puzzle

--- common.py
+++ common.py
@@ -1,6 +1,17 @@
-class NPuzzle:
+class Puzzle:
+    def __init__(self, size):
+        self.size = size
+        self.val = [[0] * size for _ in range(size)]
 
-    def __init__(self, puzzle):
-        self.puzzle = puzzle
-        self.goal = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']]
+    def set_val(self, row, col, value):
+        self.val[row][col] = value
 
+    def get_val(self, row, col):
+        return self.val[row][col]
+
+    def find_blank(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.val[i][j] == 0:
+                    return i, j
+        return None
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import common
-import method
+from math import sqrt
+
+from common import *
+from method import *
+
+goal_puzzle = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+size = int(sqrt(int(input("input the puzzle size: \n"))+1))
+print(size)
+puzzle = Puzzle(size)
+
+for i in range(size):
+    for j in range(size):
+        puzzle.set_val(i, j, value=int(input("input the value of row {} and column {}: \n".format(i, j))))
+
+blank_row, blank_col = puzzle.find_blank()
+print(blank_row, blank_col)
