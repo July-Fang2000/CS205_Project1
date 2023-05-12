61a44a587d9185a8a33dad9c04bdca8e4023664a
July-Fang2000 <zfang052@ucr.edu>
2023-05-12 01:24:25 -0700

    add: simple sample to test

--- main.py
+++ main.py
@@ -22,14 +22,22 @@ from method import *
 # goal_puzzle[size-1][size-1] = 0
 
 # use to simple test
-original_puzzle = [[0, 7, 2], [4, 6, 1], [3, 5, 8]]
+# original_puzzle = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]  # Depth 0
+# original_puzzle = [[1, 2, 3], [4, 5, 6], [0, 7, 8]]  # Depth 2
+# original_puzzle = [[1, 2, 3], [5, 0, 6], [4, 7, 8]]  # Depth 4
+# original_puzzle = [[1, 3, 6], [5, 0, 2], [4, 7, 8]]  # Depth 8
+# original_puzzle = [[1, 3, 6], [5, 0, 7], [4, 8, 2]]  # Depth 12
+# original_puzzle = [[1, 6, 7], [5, 0, 3], [4, 8, 2]]  # Depth 16
+# original_puzzle = [[7, 1, 2], [4, 8, 5], [6, 3, 0]]  # Depth 16
+original_puzzle = [[0, 7, 2], [4, 6, 1], [3, 5, 8]]  # Depth 24
+
 goal_puzzle = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
 # select the method to solve the problem
 solve_method = int(input('Select a method: \n'
-                        '1) Uniform Cost Search. \n'
-                        '2) A* with the Misplaced Tile heuristic. \n'
-                        '3) A* with the Manhattan Distance heuristic.\n'
+                         '1) Uniform Cost Search. \n'
+                         '2) A* with the Misplaced Tile heuristic. \n'
+                         '3) A* with the Manhattan Distance heuristic.\n'
                          '(e.g. if you want to use Uniform Cost Search, enter 1)\n'))
 
 # search the solution
